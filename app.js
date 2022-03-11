360d73d7032a50d6d914af31ac5dcef61ac1b108
Ebrar Erdem <ebrarerdem32@gmail.com>
2022-03-11 13:56:30 +0300

    app.js added

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const PORT = 3000;
+const app = express();
+
+const blog = { id: 1, title: "Blog title", description: "Blog description" };
+
+app.get("/",(req,res)=>{
+    res.send(blog);
+})
+
+
+
+app.listen(PORT,console.log(`listening on ${PORT}`));
