269637383be1415aa5558a37bdaae572f7d9c94f
Ebrar Erdem <ebrarerdem32@gmail.com>
2022-03-11 15:28:10 +0300

    src name changed to view

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
+// It is middleware for explain where it is
+app.use(express.static('public'));  
+
+//Use ejs engine
+app.set('view engine', 'ejs');
 
 const blog = { id: 1, title: "Blog title", description: "Blog description" };
 
