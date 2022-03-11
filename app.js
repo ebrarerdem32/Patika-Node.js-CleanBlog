bff109c7b7610e2e565959559958173dc1aaa186
Ebrar Erdem <ebrarerdem32@gmail.com>
2022-03-11 16:41:14 +0300

    app.js and json files editted

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express');
-const ejs = require('ejs');
-const app = express();
-// It is middleware for explain where it is
-app.use(express.static('public'));  
 
+const app = express();
 //Use ejs engine
 app.set('view engine', 'ejs');
 
-const blog = { id: 1, title: "Blog title", description: "Blog description" };
 
-app.get("/",(req,res)=>{
-    res.send(blog);
+// It is middleware for explain where it is
+app.use(express.static('public'));  
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+app.get('/about',(req,res)=>{
+    res.render('about');
+})
+app.get('/add_post',(req,res)=>{
+    res.render('add_post');
+})
+app.get('/post',(req,res)=>{
+    res.render('post');
 })
 
 const PORT = 3000;
